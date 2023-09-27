d283c7fac1fac9aedf06fbd4c5f39e212f310325
Admin <Admin@DESKTOP-E269TAO>
2023-09-27 14:59:14 +0530

    commit

--- wcsa5seleniumproject/src/methodsOfWebElement/ClickMethod.java
+++ wcsa5seleniumproject/src/methodsOfWebElement/ClickMethod.java
@@ -1,23 +1,39 @@
 package methodsOfWebElement;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.google.common.io.Files;
 
 public class ClickMethod {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		
-		driver.get("");
+		driver.get("file:///C:/Users/Admin/Desktop/wcsa5%20workspace/WebElement/SingleSelectDropdown.html");
+		WebElement dropDown = driver.findElement(By.id("menu"));
+		dropDown.click();
 		
+		File src = dropDown.getScreenshotAs(OutputType.FILE);
+		File dest = new File("./screenshots/dropDownss.png");
+		Files.copy(src, dest);
+		
+//		driver.findElement(By.id("menu")).click();
+		
+	}
 		
 		
 
-	}
+	
 
 }
